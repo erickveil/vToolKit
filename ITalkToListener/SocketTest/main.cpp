9aa63d6ad629337aee0046f2b1c2a5e992c8cde0
eveil <eveil@comtechcom.net>
2015-10-22 08:13:43 -0700

    Some adjustments to the test.
    Minor adjustments as the host and message are hard-coded.
    Pointed the test to several targets with success.

--- ITalkToListener/SocketTest/main.cpp
+++ ITalkToListener/SocketTest/main.cpp
@@ -3,6 +3,7 @@
 #include "fileLog.h"
 #include "TextFile.h"
 #include <QDebug>
+#include <QByteArray>
 
 using namespace vToolKit;
 
@@ -17,9 +18,13 @@ int main(int argc, char *argv[])
         fileLog log(&log_file);
         SynchronousSocketClient ssc(&log);
 
-        ssc.setConnectInfo("adsfee883.edwea:50503",5000000);
-        QString msg="test transmission";
-        QString response=ssc.sendAndReceive(msg);
+        //ssc.setConnectInfo("192.168.60.128:502",2000);
+        ssc.setConnectInfo("127.0.0.1:50503",2000);
+
+        const char  MPRR[12]={'\0','\0','\0','\0','\0','\06','\0','\03','\0','\0','\0','\01'};
+
+        QByteArray msg(MPRR,12);
+        QByteArray response=ssc.sendAndReceive(msg);
         qDebug()<<response;
     }
 
